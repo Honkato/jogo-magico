526340e78062a8dee49d1e2a6439249e1f491500
Honkato <77444910+Honkato@users.noreply.github.com>
2022-11-09 11:24:18 -0300

    Update Velha.java

--- Velha.java
+++ Velha.java
@@ -10,88 +10,131 @@ public class Velha {
             {1, 2, 3}
     };
 //    GETTERS
+
 //    GET LINES
-    int[] getLine0(){
-        return new int[]{cube[0][0], cube[0][1], cube[0][2]};
+    int[] getLine(int line){
+        if (line == 0){
+//            First line
+            return new int[]{
+                    cube[0][0],
+                    cube[0][1],
+                    cube[0][2]};
+        }else if (line == 1){
+//            Second line
+            return new int[]{
+                    cube[1][0],
+                    cube[1][1],
+                    cube[1][2]};
+        }else{
+//            Third line
+            return new int[]{
+                    cube[2][0],
+                    cube[2][1],
+                    cube[2][2]};
+        }
+
     }
 
-    int[] getLine1(){
-        return new int[]{cube[1][0], cube[1][1], cube[1][2]};
-    }
-
-    int[] getLine2(){
-        return new int[]{cube[2][0], cube[2][1], cube[2][2]};
-    }
 
 //    GET COLUMNS
-    int[] getColumn0(){
-        return new int[]{cube[0][0], cube[1][0], cube[2][0]};
+    int[] getColumn(int column){
+        if (column == 0){
+//            First column
+            return new int[]{
+                    cube[0][0],
+                    cube[1][0],
+                    cube[2][0]};
+        }else if (column == 1){
+//            Second column
+            return new int[]{
+                    cube[0][1],
+                    cube[1][1],
+                    cube[2][1]};
+        }else{
+//            Third column
+            return new int[]{
+                    cube[0][2],
+                    cube[1][2],
+                    cube[2][2]};
+        }
     }
 
-    int[] getColumn1(){
-        return new int[]{cube[0][1], cube[1][1], cube[2][1]};
-    }
-
-    int[] getColumn2(){
-        return new int[]{cube[0][2], cube[1][2], cube[2][2]};
-    }
 
 //    GET DIAGONALS
-    int[] getDiagonal0(){
-        return new int[]{cube[0][0], cube[1][1], cube[2][2]};
+    int[] getDiagonal(int diagonal){
+        if (diagonal == 0){
+            return new int[]{
+//                    Diagonal left right \
+                    cube[0][0],
+                    cube[1][1],
+                    cube[2][2]};
+        }else{
+            return new int[]{
+//                    Diagonal right left /
+                    cube[2][0],
+                    cube[1][1],
+                    cube[0][2]};
+        }
+
     }
 
-    int[] getDiagonal1(){
-        return new int[]{cube[2][0], cube[1][1], cube[0][2]};
-    }
+
 //    SETTERS
 //    SET LINES
-    void setLine0(int[] set){
-        cube[0][0] = set[0];
-        cube[0][1] = set[1];
-        cube[0][2] = set[2];
-    }
-    void setLine1(int[] set){
-        cube[1][0] = set[0];
-        cube[1][1] = set[1];
-        cube[1][2] = set[2];
-    }
-    void setLine2(int[] set){
-        cube[2][0] = set[0];
-        cube[2][1] = set[1];
-        cube[2][2] = set[2];
+    void setLine(int line,int[] set){
+        if (line == 0){
+//            First line
+            cube[0][0] = set[0];
+            cube[0][1] = set[1];
+            cube[0][2] = set[2];
+        }else if (line == 1){
+//            Second line
+            cube[1][0] = set[0];
+            cube[1][1] = set[1];
+            cube[1][2] = set[2];
+        }else{
+//            Third line
+            cube[2][0] = set[0];
+            cube[2][1] = set[1];
+            cube[2][2] = set[2];
+        }
     }
 
+
 //    SET COLUMNS
-    void setColumn0(int[] set){
-        cube[0][0] = set[0];
-        cube[1][0] = set[1];
-        cube[2][0] = set[2];
+    void setColumn(int column,int[] set){
+        if (column == 0){
+//            First column
+            cube[0][0] = set[0];
+            cube[1][0] = set[1];
+            cube[2][0] = set[2];
+        }else if (column == 1){
+//            Second column
+            cube[0][1] = set[0];
+            cube[1][1] = set[1];
+            cube[2][1] = set[2];
+        }else{
+//            Third column
+            cube[0][2] = set[0];
+            cube[1][2] = set[1];
+            cube[2][2] = set[2];
+        }
     }
-    void setColumn1(int[] set){
-        cube[0][1] = set[0];
-        cube[1][1] = set[1];
-        cube[2][1] = set[2];
-    }
-    void setColumn2(int[] set){
-        cube[0][2] = set[0];
-        cube[1][2] = set[1];
-        cube[2][2] = set[2];
-    }
+
 
 
     public boolean verifyWinner(){
 
-        int[] c0 = getColumn0();
-        int[] c1 = getColumn1();
-        int[] c2 = getColumn2();
+        int[] c0 = getColumn(0);
+        int[] c1 = getColumn(1);
+        int[] c2 = getColumn(2);
 
-        int[] l0 = getLine0();
-        int[] l1 = getLine1();
-        int[] l2 = getLine2();
+        int[] l0 = getLine(0);
+        int[] l1 = getLine(1);
+        int[] l2 = getLine(2);
 
-        int[] d0 = getDiagonal0();
-        int[] d1 = getDiagonal1();
+        int[] d0 = getDiagonal(0);
+        int[] d1 = getDiagonal(1);
 
         int[][] tudo = {l0,l1,l2, c0,c1,c2, d0,d1};
 
@@ -127,7 +170,19 @@ public class Velha {
         NÃO VERIFICA PQ NÃO PODE DAR VELHA
         */
       return false;
-    };
+    }
 //int[][] upCubo, int[][] downCubo, int[][] leftCubo, int[][] rightCubo,
+    void showCube(){
+        String formated = Arrays.deepToString(cube);
+        formated = formated.replace("[","")
+                           .replace("], ","\n")
+                           .replace("]","")
+                           .replace(", ","  ");
+
+        formated = formated.replace("10", "X")
+                           .replace("20","O");
+
+        System.out.println(formated);
+    }
 
 }
